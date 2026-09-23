84992ab1a704a6a67ec5827a730120f8e4384edd
varad-desaavle <desavalevarad1@gmail.com>
2026-09-23 18:49:28 +0530

    Fix dashboard build errors

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -28,7 +28,7 @@ const Menu = () => {
 
     return (
         <div className="menu-container">
-            <img src="logo.png" style={{ width: "50px" }} />
+            <img src="logo.png" alt="Zerodha logo" style={{ width: "50px" }} />
             <div className="menus">
                 <ul>
                     <li>
